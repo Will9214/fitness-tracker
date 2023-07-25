cc3a51eeea4fe5c99537713e0b89f13e98f5075a
Will9214 <129124730+Will9214@users.noreply.github.com>
2023-07-25 14:29:30 -0400

    added authentication code using passport. Created signin and signup POST routes and current_user GET route.

---
+++ server/controllers/authentication.js
@@ -0,0 +1,60 @@
+const jwt = require("jwt-simple");
+const keys = require("../config/dev");
+const User = require("../models/user");
+
+function tokenForUser(user) {
+  return jwt.encode({ sub: user.id,
+  iat: Math.round(Date.now() / 1000),
+  exp: Math.round(Date.now() / 1000 + 5 * 60 * 60)}, keys.TOKEN_SECRET)
+};
+
+exports.signUp = function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(422).send({ error: "You must provide username and password" })
+  }
+
+  User.findOne({ username: username }).then((result) => {
+   
+    if (result) {
+      return res.status(422).send({ error: "Username is in use. Please create a unique Username." });
+    }
+
+    const user = new User({
+      username: req.body.username,
+      activities: [],
+      workouts: [],
+      objectives: [],
+    });
+
+    user.setPassword(password);
+
+    user.save().then(() => {
+      res.json({ token: tokenForUser(user), user })
+    })
+
+  });
+};
+
+exports.signIn = async function (req, res, next) {
+  // User has already had their email and password auth'd
+  // Just need to give them a token
+  res.send({
+    token: tokenForUser(user),
+  })
+};
+
+exports.currentUser = function(req, res) {
+  const user = {
+    username: req.user.username,
+    token: tokenForUser(req.user),
+    // activities
+    // workouts
+    // objectives
+    // groups
+  };
+
+  res.send(user);
+};
---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const crypto = require("crypto");
+
+// Define Model
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  activities: [{ type: Schema.Types.ObjectId, ref: "activities" }],
+  workouts: [{ type: Schema.Types.ObjectId, ref: "workouts" }],
+  objectives: [{ type: Schema.Types.ObjectId, ref: "objectives" }],  
+  hash: String,
+  salt: String,
+});
+
+UserSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
+};
+
+UserSchema.methods.validPassword = function(password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
+
+  return this.hash === hash;
+};
+
+module.exports = mongoose.model("user", UserSchema);
