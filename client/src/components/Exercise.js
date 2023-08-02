57eebd1e026f69881daebe05b6df7de4f761348f
Will9214 <129124730+Will9214@users.noreply.github.com>
2023-08-02 12:38:54 -0400

    added react-bootstrap Placeholder components to be visisble when results are still loading.

--- client/src/components/Exercise.js
+++ client/src/components/Exercise.js
@@ -1,34 +1,64 @@
 import React, { useState, Fragment } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { Placeholder } from "react-bootstrap";
 
 const Exercise = ({ id, name, type, muscle, equipment, difficulty, instructions }) => {
+
+  const { loading } = useSelector(state => state.exerciseApi);
 
   const [showMore, setShowMore] = useState(false);
   const handleShowMoreClick = () => setShowMore(true);
   const handleShowLessClick = () => setShowMore(false);
+  // const [isLoading, setLoading] = useState(loading);
 
   return (
-    <ResultsContainer className="container">
-      <Fragment>
-        <Name><strong>Name:</strong> {name}</Name>
-        <Type><strong>Type:</strong> {type}</Type>
-        <Muscle><strong>Muscle:</strong> {muscle}</Muscle>
-        <Equipment><strong>Equipment:</strong> {equipment}</Equipment>
-        <Difficulty><strong>Difficulty:</strong> {difficulty}</Difficulty>
-        {showMore ? (
+    <Fragment>
+    {loading ? (
+      <ResultsContainer className="container">
+        <Placeholder as={Name} animation="glow">
+          <Placeholder xs={4} />
+        </Placeholder>
+        <Placeholder as={Type} animation="glow">
+          <Placeholder xs={3}/>
+        </Placeholder>
+        <Placeholder as={Muscle} animation="glow">
+          <Placeholder xs={3}/>
+        </Placeholder>
+        <Placeholder as={Equipment} animation="glow">
+          <Placeholder xs={3}/>
+        </Placeholder>
+        <Placeholder as={Difficulty} animation="glow">
+          <Placeholder xs={3}/>
+        </Placeholder>
+        <Placeholder as={TruncatedInstructions} animation="glow">
+          <Placeholder xs={11}/>
+        </Placeholder>
+      </ResultsContainer>
+    ) : (
+      <ResultsContainer className="container">
         <Fragment>
-          <FullInstructions><strong>Instructions:</strong> {instructions}</FullInstructions>
-          <ShowLess onClick={handleShowLessClick}>...Show Less</ShowLess>
-        </Fragment>
-        ) : (
-        <Fragment>
-          <TruncatedInstructions><strong>Instructions:</strong> {instructions}</TruncatedInstructions>
-          <ShowMore onClick={handleShowMoreClick}>...Show More</ShowMore>
-        </Fragment>
+          <Name><strong>Name:</strong> {name}</Name>
+          <Type><strong>Type:</strong> {type}</Type>
+          <Muscle><strong>Muscle:</strong> {muscle}</Muscle>
+          <Equipment><strong>Equipment:</strong> {equipment}</Equipment>
+          <Difficulty><strong>Difficulty:</strong> {difficulty}</Difficulty>
+          {showMore ? (
+          <Fragment>
+            <FullInstructions><strong>Instructions:</strong> {instructions}</FullInstructions>
+            <ShowLess onClick={handleShowLessClick}>...Show Less</ShowLess>
+          </Fragment>
+          ) : (
+          <Fragment>
+            <TruncatedInstructions><strong>Instructions:</strong> {instructions}</TruncatedInstructions>
+            <ShowMore onClick={handleShowMoreClick}>...Show More</ShowMore>
+          </Fragment>
         )}
-      </Fragment>
-    </ResultsContainer>
+        </Fragment>
+      </ResultsContainer>
+    )}
+    </Fragment>
+
   )
 
 };
