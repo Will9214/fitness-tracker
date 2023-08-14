702466764111a1e017e5082af1a7e7d5427f0a3b
Will9214 <129124730+Will9214@users.noreply.github.com>
2023-08-14 13:05:41 -0400

    reversed completedWorkouts array so that the most recent completed workout is displayed first.

--- client/src/components/ShowCompletedWorkouts.js
+++ client/src/components/ShowCompletedWorkouts.js
@@ -7,12 +7,14 @@ import CompletedWorkout from "./CompletedWorkout";
 const ShowCompletedWorkouts = () => {
   const { completedWorkouts } = useSelector(state => state.userWorkouts);
 
+  const reversedCompletedWorkoutsArr = completedWorkouts.toReversed();
+
   return (
     <CompletedWorkoutsContainer className="col-sm-6 offset-sm-3">
       <div className="display-6 text-center">Completed Workouts</div>
       <hr className="m-1" />
 
-      {completedWorkouts?.map((workout, i) => (
+      {reversedCompletedWorkoutsArr?.map((workout, i) => (
         <CompletedWorkout
           key={workout._id}
           id={workout._id}
